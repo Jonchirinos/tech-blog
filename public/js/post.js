4577f09bb34ee14a91a38d404b4568e234bf0f0e
Jon Chirinos <chirinosjon@gmail.com>
2022-04-12 01:20:41 -0500

    still working on comment functionality

--- public/js/post.js
+++ public/js/post.js
@@ -1,4 +1,4 @@
-const newFormHandler = async (event) => {
+const newCommentHandler = async (event) => {
   event.preventDefault();
   alert('you made it here');
 
@@ -23,5 +23,5 @@ const newFormHandler = async (event) => {
 };
 
 document
-  .querySelector('.new-post-form')
-  .addEventListener('submit', newFormHandler);
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', newCommentHandler);
